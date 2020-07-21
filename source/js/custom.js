a9bb318565b948d66d7544e3794b381996ba5864
GITEE <gitee@wujiaxing.cn>
2020-07-21 22:19:24 +0800

    update

---
+++ source/js/custom.js
@@ -0,0 +1,68 @@
+function wait(callback, seconds) {
+    var timelag = null;
+    timelag = window.setTimeout(callback, seconds);
+}
+var initCopyCode = function(){
+    var copyHtml = '';
+    copyHtml += '<button class="btn-copy" data-clipboard-snippet="">';
+    copyHtml += '<i class="fas fa-copy"></i><span>复制</span>';
+    copyHtml += '</button>';
+    $(".highlight .code pre").before(copyHtml);
+    // $(".article pre code").before(copyHtml);
+    var clipboard = new ClipboardJS('.btn-copy', {
+    target: function(trigger) {
+        return trigger.nextElementSibling;
+    }
+    });
+    clipboard.on('success', function(e) {
+    let $btn = $(e.trigger);
+    $btn.addClass('copied');
+    let $icon = $($btn.find('i'));
+    $icon.removeClass('fa-copy');
+    $icon.addClass('fa-check-circle');
+    let $span = $($btn.find('span'));
+    $span[0].innerText = '复制完成';
+    
+    wait(function () { // 等待两秒钟后恢复
+        $icon.removeClass('fa-check-circle');
+        $icon.addClass('fa-copy');
+        $span[0].innerText = '复制';
+    }, 2000);
+    });
+    clipboard.on('error', function(e) {
+    e.clearSelection();
+    let $btn = $(e.trigger);
+    $btn.addClass('copy-failed');
+    let $icon = $($btn.find('i'));
+    $icon.removeClass('fa-copy');
+    $icon.addClass('fa-times-circle');
+    let $span = $($btn.find('span'));
+    $span[0].innerText = '复制失败';
+    
+    wait(function () { // 等待两秒钟后恢复
+        $icon.removeClass('fa-times-circle');
+        $icon.addClass('fa-copy');
+        $span[0].innerText = '复制';
+    }, 2000);
+    });
+}
+//加载页面后自动调用方法
+function onload_content(){
+    //查看大图
+    $('.article-entry').each(function(i){
+        $(this).find('img').each(function(){
+            if ($(this).parent().hasClass('fancybox')) return;
+            var alt = this.alt;
+            if (alt) $(this).after('<span class="caption">' + alt + '</span>');
+            $(this).wrap('<a href="' + this.src + '" title="' + alt + '" class="fancybox"></a>');
+        });
+        $(this).find('.fancybox').each(function(){
+            $(this).attr('rel', 'article' + i);
+        });
+    });
+    if ($.fancybox){
+        $('.fancybox').fancybox();
+    }
+    //创建复制按钮
+    initCopyCode();
+}
